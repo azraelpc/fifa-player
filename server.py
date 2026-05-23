ec734771dd2fae9f0ab45dc1c00baf7fad4bf6f9
azraelpc <winsms@gmail.com>
2026-05-23 05:08:43 +0200

    bugfix on cache - was not refreshing correctly

--- server.py
+++ server.py
@@ -130,7 +130,8 @@ class MusicServerHandler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.send_header('Content-Type', 'application/json; charset=utf-8')
                 self.end_headers()
-                self.wfile.write(json.dumps(CACHED_LIBRARY).encode('utf-8'))
+                data = scan_music()
+                self.wfile.write(json.dumps(data).encode('utf-8'))
                 return
 
             # Servir ficheros de música/imágenes
